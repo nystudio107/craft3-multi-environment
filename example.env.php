bbdfa1eb90c4aae3d209aea7ad8837373bc562bf
Andrew Welch <andrew@megalomaniac.com>
2017-02-20 14:34:25 -0500

    * Added {}

--- example.env.php
+++ example.env.php
@@ -61,7 +61,7 @@ $craftenv_vars = [
 
 // Set all of the .env values, auto-prefixed with `CRAFTENV_`
 foreach ($craftenv_vars as $key => $value) {
-    putenv("CRAFTENV_$key=$value");
+    putenv("CRAFTENV_{$key}={$value}");
 }
 
 /**
