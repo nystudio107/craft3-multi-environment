b00685371491d5448c8f49770fa82b0df2548aed
Dennis Frank <github@dennisfrank.info>
2019-02-03 13:14:06 +0100

    Added useProjectConfigFile

--- config/general.php
+++ config/general.php
@@ -38,6 +38,7 @@ return [
         'siteUrl' => getenv('CRAFTENV_SITE_URL'),
         'useEmailAsUsername' => true,
         'usePathInfo' => true,
+        'useProjectConfigFile' => true,
         // Aliases parsed in sites’ settings, volumes’ settings, and Local volumes’ settings
         'aliases' => [
             '@basePath' => getenv('CRAFTENV_BASE_PATH'),
